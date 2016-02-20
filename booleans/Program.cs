6f951cffc8b9403fc193d1927a30970cf234b922
AlexBennett <alexanderbondbennett@gmail.com>
2016-02-20 12:13:48 -0500

    used boolean logic in string interpolation

---
+++ booleans/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace booleans
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			bool content = true;
+			bool noContent = false;
+
+			Console.WriteLine("It is {0} that C# provides programming language content.", content);
+			Console.WriteLine("That is not {0}.", noContent);
+		}
+	}
+}
